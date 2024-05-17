39d37db3b8d0fb15750da18e4a34dc97fdf4ea5d
krishna <79183768+krishnakumarbhat@users.noreply.github.com>
2024-05-17 21:46:35 +0530

    Update main.py

--- rasppi/main.py
+++ rasppi/main.py
@@ -33,7 +33,9 @@ def capture_image():
 def get_device_ip(mac_address):
     """Get the IP address associated with a given MAC address."""
     mac_address = mac_address.lower().replace('-', ':')
+    print("hesdfsdfllo")
     arp_output = subprocess.check_output(["arp", "-a"]).decode()
+    print("hesdfo")
     for line in arp_output.split('\n'):
         if mac_address in line:
             ip_address_match = re.search(r'(\d+\.\d+\.\d+\.\d+)', line)
