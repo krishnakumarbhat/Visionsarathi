70e3d43d6f103efd74cb8f1bed52306df88d9ecb
krishna <79183768+krishnakumarbhat@users.noreply.github.com>
2024-05-17 08:21:14 +0530

    Update main.py

--- rasppi/main.py
+++ rasppi/main.py
@@ -125,7 +125,7 @@ def main():
 
             if gpio_state_sound == GPIO.LOW:
                 print("GPIO pin is LOW. Playing sound...")
-                # play_audio("audio.mp3")
+                play_audio("audio.mp3")
 
             time.sleep(1)
 
