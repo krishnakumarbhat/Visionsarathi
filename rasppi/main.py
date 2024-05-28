1d7cdf2f58793bec9728084d9ca187a9f7db7a51
krishna <79183768+krishnakumarbhat@users.noreply.github.com>
2024-05-28 20:13:16 +0530

    Update main.py

--- rasppi/main.py
+++ rasppi/main.py
@@ -6,27 +6,18 @@ import RPi.GPIO as GPIO
 import time
 import os
 import pygame
-# import siu 
 
 # Set the GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pins
 gpio_pin_capture = 26
-gpio_pin_sound = 21  # Connected to Amp DIN
 
 # Setup the GPIO pins
 GPIO.setup(gpio_pin_capture, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(gpio_pin_sound, GPIO.OUT)
 
 def capture_image():
     """Capture an image using fswebcam and save it as img.png."""
-    # Use raspistill command to capture an image and store it as img.png
-    # subprocess.run(["raspistill", "-o", "img.png"])
-    # subprocess.run(["fswebcam -r 1280x720 --no-banner","img.png"])
-
-    # Run the command using os.system
-    # os.system("fswebcam -r 640x480 -b MJPEG --no-banner img.png")
     os.system("raspistill -o img.png")
 
 def get_device_ip(mac_address):
@@ -37,10 +28,9 @@ def get_device_ip(mac_address):
         if mac_address in line:
             ip_address_match = re.search(r'(\d+\.\d+\.\d+\.\d+)', line)
             if ip_address_match:
-                print(ip_address_match)
                 return ip_address_match.group(1)
     return None
-    
+
 def recv_msg(sock):
     """Receive a message from the socket."""
     raw_msglen = recvall(sock, 4)
@@ -48,7 +38,6 @@ def recv_msg(sock):
         return None
     msglen = struct.unpack('>I', raw_msglen)[0]
     return recvall(sock, msglen)
-
 def recvall(sock, n):
     """Receive n bytes from the socket."""
     data = bytearray()
@@ -59,9 +48,8 @@ def recvall(sock, n):
         data.extend(packet)
     return data
 
-
 def send_image_and_receive_audio(ip_address):
-    """Send an image to the server and receive audio in response."""
+    """Send an image to the server and receive two audio files in response."""
     if ip_address is None:
         print("Device with the specified MAC address not found in the network.")
         return
@@ -78,61 +66,49 @@ def send_image_and_receive_audio(ip_address):
             client_socket.sendall(struct.pack("Q", len(image_data)) + image_data)
             print("Image sent to server")
 
-        # Receive the audio from the server
-        received_data = recv_msg(client_socket)
+        # Receive the first audio file from the server
+        received_data1 = recv_msg(client_socket)
+        audio_file_path1 = "audio.mp3"
+        with open(audio_file_path1, "wb") as audio_file1:
+            audio_file1.write(received_data1)
+        print(f"Received first audio file: {audio_file_path1}")
 
-        # Handle audio format (replace with your specific format)
-        audio_format = "mp3"
-        audio_file_path = f"audio.{audio_format}"
-        with open(audio_file_path, "wb") as audio_file:
-            audio_file.write(received_data)
-        print(f"Received audio data (format: {audio_format})")
-        
-        return audio_file_path
+        # Receive the second audio file from the server
+        received_data2 = recv_msg(client_socket)
+        audio_file_path2 = "audio_kan.mp3"
+        with open(audio_file_path2, "wb") as audio_file2:
+            audio_file2.write(received_data2)
+        print(f"Received second audio file: {audio_file_path2}")
+        return audio_file_path1, audio_file_path2
 
     except Exception as e:
         print("Error:", e)
     finally:
         client_socket.close()
 
-# def play_audio(music_file_path):
-#     """Play the audio file."""
-#     pygame.mixer.init()
-#     pygame.mixer.music.load(music_file_path)
-#     pygame.mixer.music.set_volume(0.7)
-#     pygame.mixer.music.play()
-#     while pygame.mixer.music.get_busy():
-#         continue
-#         # time.sleep(1)
-
-
-
 def main():
     try:
         while True:
             gpio_state_capture = GPIO.input(gpio_pin_capture)
-            gpio_state_sound = GPIO.input(gpio_pin_sound)
-
             if gpio_state_capture == GPIO.LOW:
                 print("GPIO pin is LOW. Capturing and sending image...")
                 capture_image()
-                print("hello")
                 mac_address = "C0-A5-E8-6F-94-E7"
                 ip_address = get_device_ip(mac_address)
-                print(ip_address)
-                audio_file_path = send_image_and_receive_audio(ip_address)
-                print(audio_file_path)
-            if gpio_state_sound == GPIO.LOW:
-                print("GPIO pin is LOW. Playing sound...")
-                os.system("python siu.py")
-
+                audio_file_paths = send_image_and_receive_audio(ip_address)
+                if audio_file_paths:
+                    for audio_file_path in audio_file_paths:
+                        print(f"Audio file saved: {audio_file_path}")
+                os.system("python sp.py")
 
             time.sleep(1)
 
     except KeyboardInterrupt:
         print("Exiting...")
     finally:
+        print("in finally")
         GPIO.cleanup()
 
 if __name__ == "__main__":
     main()
+
